d62b8e6e56cd6823e95febb295a1aafd1706dc73
gaetan-che <gaetan.chevalley@heig-vd.ch>
2013-08-08 17:58:48 +0200

    Adding update of the server and a small readme about the dialog between client app and server

--- server_hangman/src/Server.java
+++ server_hangman/src/Server.java
@@ -3,12 +3,12 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.util.LinkedList;
+import java.util.Random;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 
 public class Server extends HttpServlet {
 
@@ -18,10 +18,10 @@ public class Server extends HttpServlet {
 	// The current state of the word to find
 	private String state;
 	// Number of attempts left
-	private int left;
-	
+	private int attemptLeft;
+
 	private LinkedList<String> wordList = new LinkedList<String>();
-	
+
 	{
 		wordList.add("ANDROID");
 		wordList.add("CALIFORNIA");
@@ -30,36 +30,62 @@ public class Server extends HttpServlet {
 		wordList.add("CHEESEBURGER");
 		wordList.add("UNBEATABlE");
 		wordList.add("VOCABULARY");
-		
 	}
 
 	// Get method
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html");
-		
+
 		// Read the character entered by the player
 		BufferedReader in = request.getReader();
-		String l = in.readLine();	
+		String l = in.readLine();
 		in.close();
-		
+
+		// Format the response
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(
+				response.getOutputStream(), "UTF-8"));
+
 		// Get the letter parameter and handle it
 		String letter = (String) request.getParameter("letter");
 		if (letter != null) {
+			// Check if a game was created first
+			if (word != null) {
 
+				// Check if there is still some attempt left
+				if (attemptLeft > 0) {
+					if (word.contains(letter)) {
+
+						// Update the state of the guessed word
+						for (int i = 0; i < word.length(); i++) {
+							if (word.charAt(i) == letter.charAt(0)) {
+								state.toCharArray()[i] = letter.charAt(0);
+							}
+						}
+					} else {
+						attemptLeft--;
+					}
+					out.write(attemptLeft);
+				} else {
+					out.write("game-over");
+				}
+			}
 		}
-		
+
 		// Get the new parameter and handle it
 		String newGame = (String) request.getParameter("new");
 		if (newGame != null) {
-			
+			Random rand = new Random();
+			int index = rand.nextInt(wordList.size());
+			word = wordList.get(index);
+			state = "";
+			// Initialisation of the current state (empty word)
+			for (int i = 0; i < word.length(); i++)
+				state += "_";
+
+			out.write("game-start");
 		}
-		
-		// Format the response
-		BufferedWriter out = new BufferedWriter(
-				new OutputStreamWriter(response.getOutputStream(),
-						"UTF-8"));
-		out.write("Salut");
+
 		out.close();
 	}
 }
